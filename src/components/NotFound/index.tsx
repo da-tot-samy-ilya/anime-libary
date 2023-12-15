f306e4edefca5a1656a3f5c88d2298bf35007db2
Ilya <bannikov_iv@sima-land.ru>
2023-12-15 17:31:49 +0500

    Добавил роутер и страницы

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import {Menu} from "../Menu";
+import {Typography} from "@mui/material";
+
+export const NotFound = () => {
+  return (
+    <>
+      <Menu />
+      <Typography>Not Found 404</Typography>
+    </>
+  );
+};
---
+++ src/routes/Root/index.tsx
@@ -0,0 +1,5 @@
+import {Menu} from "../../components/Menu";
+
+export const Root = () => {
+    return <><Menu/></>
+}
