de255c324771e08342345a6385e5b7cdac50b588
Ilya <bannikov_iv@sima-land.ru>
2023-12-17 00:57:32 +0500

    Добавил компонент выпадающего меню

---
+++ src/components/MenuDropDown/index.tsx
@@ -0,0 +1,52 @@
+import { Button, Link, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+
+export interface DropDownList {
+  name: string;
+  link: string;
+}
+
+interface Props {
+  name: string;
+  list: DropDownList[] | null;
+}
+
+export function MenuDropDown({ name, list }: Props) {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <>
+        <Button
+          aria-owns={anchorEl ? "simple-menu" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handleClick}
+        >
+          {name}
+        </Button>
+
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          onMouseLeave={handleClose}
+        >
+          {list?.map((item) => (
+            <Link underline="none" href={`categories/${item.link}`}>
+              <MenuItem onClick={handleClose}>{item.name}</MenuItem>
+            </Link>
+          ))}
+        </Menu>
+      </>
+    </div>
+  );
+}
